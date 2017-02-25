6c106144cc2c49f2e4525b81c90882bb75664e47
wt <wt@LAPTOP-TDN64PBM>
2017-02-25 21:47:32 +0800

    提交

---
+++ test/com/alibab/study/test/LoggerTest.java
@@ -0,0 +1,24 @@
+package com.alibab.study.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.alibaba.study.Log4jDemo;
+
+public class LoggerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("֮ǰ");
+	}
+
+	@Test
+	public void testLoggerStudy() {
+		//fail("Not yet implemented");
+		Log4jDemo ld = new Log4jDemo();
+		ld.loggerStudy();
+	}
+
+}
