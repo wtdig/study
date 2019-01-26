b15c0475c6d41a7ca28be1f3016733b821035315
wtgit <yuyedewutong@icloud.com>
2019-01-26 15:18:23 +0800

    test

--- src/com/alibaba/study/Log4jDemo.java
+++ src/com/alibaba/study/Log4jDemo.java
@@ -5,27 +5,27 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class Log4jDemo {
-	// ��ȡlogger��־����
-	static Logger logger = Logger.getLogger(Log4jDemo.class);
+    // 获取logger日志对象
+    static Logger logger = Logger.getLogger(Log4jDemo.class);
 
-	public static void main(String[] args) {
-		// Ĭ�ϼ��ص�������
-		//BasicConfigurator.configure();
-		//������־����
-		//logger.setLevel(Level.ERROR);
-		logger.trace("trace");
-		logger.debug("debug");
-		logger.info("info");
-		logger.warn("warn");
-		logger.error("error");
-		logger.fatal("fatal");
-	}
-	
-	//��ӡlogger��־�ķ���
-	public void loggerStudy(){
-		logger.info("����info");
-		logger.debug("����debug");
-		System.out.println("��ӡ��־1234");
-		System.out.println("��ӡ��־devops");
-	}
+    public static void main(String[] args) {
+        // 默认加载的配置项
+        //BasicConfigurator.configure();
+        //设置日志级别
+        //logger.setLevel(Level.ERROR);
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.fatal("fatal");
+    }
+
+    //打印logger日志的方法
+    public void loggerStudy(){
+        logger.info("这是info");
+        logger.debug("这是debug");
+        System.out.println("打印日志123");
+        System.out.println("打印日志devops");
+    }
 }
